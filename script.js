93449c57526d619ddf8a4212b74f8bdc6b0ecb0e
Your Name <you@example.com>
2024-10-23 18:05:52 -0700

    added console logs

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == 0);
+console.log(5 > 2 && 6 > 3);
+console.log( false == false);
+console.log(true || false);
